e315e9b8c6348ea118a478be8c7702cc10565cbd
Mingw0717-Dell <xmw0517@gmail.com>
2021-09-04 21:38:21 +0800

    master -> myExtends module commit 1.0

---
+++ myExtends/src/com/itheima_01/Fu.java
@@ -0,0 +1,15 @@
+package com.itheima_01;
+
+/**
+ * 傅
+ *
+ * @author DELL_
+ * @date 2021-09-04
+ */
+
+
+public class Fu {
+    public static void show() {
+        System.out.println("show方法被调用");
+    }
+}
---
+++ myExtends/src/com/itheima_01/Zi.java
@@ -0,0 +1,15 @@
+package com.itheima_01;
+
+/**
+ * 子
+ *
+ * @author DELL_
+ * @date 2021-09-04
+ */
+
+
+public class Zi extends Fu{
+    public static void method() {
+        System.out.println("method方法被调用");
+    }
+}
