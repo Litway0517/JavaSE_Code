1c289e3aa4b87ce54375180dced8ddbb271ae994
Mingw0717-Dell <xmw0517@gmail.com>
2021-09-06 21:37:17 +0800

    master -> myExtends module commit 1.3

--- myExtends/src/com/itheima_02/Zi.java
+++ myExtends/src/com/itheima_02/Zi.java
@@ -12,10 +12,10 @@ package com.itheima_02;
 public class Zi extends Fu{
     // 身高
     public int height = 177;
-//    public int age = 20;
+    public int age = 20;
 
     public void show() {
-//        int age = 30;           // 变量的范围, 首先在方法内寻找, 再去本身的类内去寻找, 最后到父类去寻找
+        int age = 30;           // 变量的范围, 首先在方法内寻找, 再去本身的类内去寻找, 最后到父类去寻找
         System.out.println(age);
         System.out.println(height);
     }
