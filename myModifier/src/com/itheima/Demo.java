9c8d2817706eb890fa3b32626b1bb4a0dccf2645
Mingw0717-Dell <xmw0517@gmail.com>
2021-09-06 22:28:20 +0800

    master -> myModifier module commit 1.1

--- myModifier/src/com/itheima/Demo.java
+++ myModifier/src/com/itheima/Demo.java
@@ -10,6 +10,7 @@ public class Demo {
     public static void main(String[] args) {
 
         // 创建Fu类的对象, 注意需要导包
+        // 不同包的无关类, 只能调用公用方法, 即public修饰的方法
         Fu f = new Fu();
         f.show4();
 
