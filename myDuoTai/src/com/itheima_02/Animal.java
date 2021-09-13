6e99c5e6e245f34c56dd7444648bd9804e794936
Mingw0717-Dell <xmw0517@gmail.com>
2021-09-13 23:25:24 +0800

    master -> myDuoTai module commit 1.2

---
+++ myDuoTai/src/com/itheima_02/Animal.java
@@ -0,0 +1,11 @@
+package com.itheima_02;
+
+public class Animal {
+
+    public int age = 40;
+
+    // 定义 成员方法
+    public void eat() {
+        System.out.println("动物吃东西");
+    }
+}
---
+++ myDuoTai/src/com/itheima_02/AnimalDemo.java
@@ -0,0 +1,22 @@
+package com.itheima_02;
+
+
+/*
+    测试类
+ */
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+
+        // 有父类引用指向子类对象
+        Animal a = new Cat();
+
+        System.out.println(a.age);      // 成员变量a的编译看左面, 运行看左面. 因为Animal里面的age为40, 所以输出40.
+//        System.out.println(a.weight); // 因为父类Animal里面没有weight, 所以不能访问.
+
+
+        // 同理, 方法也是看左面:
+        a.eat();        // 多态的方法, 编译看左面, 运行看右面.
+//        a.playGame();     // 同样报错, 编译就出错了. 因为Animal类没有playGame()这个成员方法
+    }
+}
