9158b5c8ad76c8106ebedb16c421a6a93e390849
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-09 00:00:59 +0800

    master -> myReflect module commit 2.3(05)

--- myReflect/src/com/itheima_05/ReflectDemo02.java
+++ myReflect/src/com/itheima_05/ReflectDemo02.java
@@ -1,6 +1,7 @@
 package com.itheima_05;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -23,7 +24,7 @@ import java.lang.reflect.Method;
 
 public class ReflectDemo02 {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
-            IllegalAccessException, InvocationTargetException, InstantiationException {
+            IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
 
         // 获取class对象
         Class<?> s = Class.forName("com.itheima_02.Student");
@@ -51,6 +52,18 @@ public class ReflectDemo02 {
         Method declaredFunc = s.getDeclaredMethod("function");
         declaredFunc.setAccessible(true);
         declaredFunc.invoke(obj);
+
+
+        // 获取成员变量和成员方法
+        Field ageField = s.getDeclaredField("age");
+        ageField.setAccessible(true);
+        ageField.set(obj, 33);
+
+        Method method2 = s.getMethod("method2", String.class);
+        method2.invoke(obj, "这里需要传入参数, 别忘了");
+
+        System.out.println(obj);
+
 
 
     }
