1f6ef36727febe442577886fd97f157364e38e04
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-08 23:40:08 +0800

    master -> myReflect module commit 1.9(05)

---
+++ myReflect/src/com/itheima_05/ReflectDemo02.java
@@ -0,0 +1,57 @@
+package com.itheima_05;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 反映demo02
+ *
+ * @author DELL_
+ * @date 2021-08-13
+ */
+
+/*
+    练习：通过反射实现如下操作
+        Students=new Student（）；
+        s.method1（）；
+        s.method2（“林青霞”）；
+        String ss=s.method3（"林青霞"，30）；
+        System.out.println（ss）；
+        s.function（）；
+ */
+
+public class ReflectDemo02 {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException, InstantiationException {
+
+        // 获取class对象
+        Class<?> s = Class.forName("com.itheima_02.Student");
+
+        // 获取成员方法
+        Method m1 = s.getMethod("method1");
+
+        // 获取无参构造方法
+        Constructor<?> con = s.getConstructor();
+        Object obj = con.newInstance();
+
+        // 使得obj调用m1方法
+        m1.invoke(obj);
+        System.out.println("--------");
+
+        Method m2 = s.getMethod("method2", String.class);
+        m2.invoke(obj, "林青霞");
+        System.out.println("--------");
+
+        Method m3 = s.getMethod("method3", String.class, int.class);
+        Object ss = m3.invoke(obj, "林青霞", 30);
+        System.out.println(ss);
+        System.out.println("--------");
+
+        Method declaredFunc = s.getDeclaredMethod("function");
+        declaredFunc.setAccessible(true);
+        declaredFunc.invoke(obj);
+
+
+    }
+}
