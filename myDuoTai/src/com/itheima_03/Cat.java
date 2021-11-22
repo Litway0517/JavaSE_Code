4c24ee20326f980da2b6235f7d9f78b88fa50454
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-22 18:50:33 +0800

    master -> myDuoTai module 具体的动物类, 比如猪狗猫

---
+++ myDuoTai/src/com/itheima_03/Cat.java
@@ -0,0 +1,11 @@
+package com.itheima_03;
+
+
+public class Cat extends Animal {
+
+    // 定义 成员方法 --> 方法重写
+    @Override
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+}
---
+++ myDuoTai/src/com/itheima_03/Pig.java
@@ -0,0 +1,11 @@
+package com.itheima_03;
+
+public class Pig extends Animal{
+
+    // 重写方法
+
+    @Override
+    public void eat() {
+        System.out.println("猪吃白菜");
+    }
+}
