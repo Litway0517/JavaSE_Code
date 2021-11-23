dc9f8713569dabc18921e04559aa7962e683c602
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-23 22:45:53 +0800

    master -> myDuoTai module 动物类AnimalDemo

--- myDuoTai/src/com/itheima_04/AnimalDemo.java
+++ myDuoTai/src/com/itheima_04/AnimalDemo.java
@@ -14,7 +14,7 @@ public class AnimalDemo {
 
         // 多态
         Animal a = new Cat();   // 向上转型 --> 因为是右面的Cat()是赋值给Animal的.
-        a.eat();        // 编译看左边的Animal类中的eat()方法, 但是运行看右面的Cat中eat()方法.
+        a.eat();        // 编译看左边的Animal类中的eat()方法, 编译通过, 因为Animal类中有eat()方法. 但是运行看右面的Cat中eat()方法.
 //        a.playGame();   // 报错, 因为Animal类中压根没有playGame()方法, 编译不通过.
 
         System.out.println("-------------------");
