116558a1ddc25d513d92fc7579bec3ff83994384
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-26 18:02:25 +0800

    master -> myAbstract module 抽象动物类Animal和AnimalDemo

---
+++ myAbstract/src/com/itheima_01/Animal.java
@@ -0,0 +1,8 @@
+package com.itheima_01;
+
+public abstract class Animal {      // 抽象类
+
+
+    // 定义 成员方法
+    public abstract void eat();     // 抽象方法
+}
---
+++ myAbstract/src/com/itheima_01/AnimalDemo.java
@@ -0,0 +1,14 @@
+package com.itheima_01;
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+
+        // 创建对象
+        /*
+        Animal a = new Animal();
+        a.eat();
+         */
+
+        // Animal是抽象类不能被实例化.
+    }
+}
