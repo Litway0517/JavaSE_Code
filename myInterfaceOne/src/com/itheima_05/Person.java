7010fd9703fdf8d4c482b4e041983c4eec2044c3
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-28 14:19:55 +0800

    master -> myInterfaceOne module 一个抽象类Person 用于被其他更加具体的类继承

---
+++ myInterfaceOne/src/com/itheima_05/Person.java
@@ -0,0 +1,36 @@
+package com.itheima_05;
+
+public abstract class Person {
+
+    // 定义成员变量
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // get\set
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // 定义 抽象方法吃饭
+    public abstract void eat();
+}
