11e9b4b213a58ca3b328c8b4b7c8c110aa2fedd3
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-28 19:01:07 +0800

    master -> myArgs module 测试类

--- myArgs/src/com/itheima_03/JumpingDemo.java
+++ myArgs/src/com/itheima_03/JumpingDemo.java
@@ -10,7 +10,11 @@ public class JumpingDemo {
         // 创建对象
         JumpingOperator jo = new JumpingOperator();
         Jumping j = new Cat();      // 用实现了该接口的类来实例化.
+        Cat cat = new Cat();
+
+        // userJumping这个方法需要的是一个接口, 因此上面需要通过一个该接口的实现类传入. 下面两行都是正确的
         jo.useJumping(j);
+        jo.useJumping(cat);
 
         System.out.println("----------");
 
