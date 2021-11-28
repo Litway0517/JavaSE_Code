a937c6f599c66579312345465e08d2626b5109fa
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-28 18:39:05 +0800

    master -> myArgs module Cat类

---
+++ myArgs/src/com/itheima_01/Cat.java
@@ -0,0 +1,11 @@
+package com.itheima_01;
+
+public class Cat {
+
+    // 定义 成员方法
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+
+
+}
---
+++ myArgs/src/com/itheima_01/CatOperator.java
@@ -0,0 +1,19 @@
+package com.itheima_01;
+
+/*
+    操作类
+ */
+
+public class CatOperator {
+
+    // 操作猫
+    public void useCat(Cat c) {
+        c.eat();
+    }
+
+    public Cat getCat() {
+        Cat c = new Cat();
+
+        return c;
+    }
+}
