03f6ffdc1808b71ebf77a57185b7d930ecb7462d
Mingw0717-Dell <xmw0517@gmail.com>
2021-12-07 22:12:13 +0800

    master -> myStringBuilder module 重新复习以下字符串容器

--- myStringBuilder/src/com/itheima_01/StringBuilderDemo01.java
+++ myStringBuilder/src/com/itheima_01/StringBuilderDemo01.java
@@ -13,10 +13,9 @@ public class StringBuilderDemo01 {
 
         // 创建对象
         StringBuilder sb = new StringBuilder();
-        System.out.println(sb + ", " + sb.capacity());
-        System.out.println("sb: " + sb);
-        System.out.println("sb.length: " + sb.length());
-        System.out.println("sb.capacity: " + sb.capacity());    // 默认容量16
+        System.out.println("sb内容 -> " + sb);
+        System.out.println("sb.length -> " + sb.length());
+        System.out.println("sb.capacity -> " + sb.capacity());    // 默认容量16
 
         // 根据字符串内容创建StringBuilder
         String s = "hello";
--- myStringBuilder/src/com/itheima_02/StringBuilderDemo01.java
+++ myStringBuilder/src/com/itheima_02/StringBuilderDemo01.java
@@ -1,11 +1,18 @@
 package com.itheima_02;
 
+/**
+ * 字符串生成器demo01
+ *
+ * @author DELL_
+ * @date 2021/12/07
+ */
 public class StringBuilderDemo01 {
     public static void main(String[] args) {
         // 创建对象
         StringBuilder sb = new StringBuilder();
 
         // 添加数据, 并返回数据本身 --> 就是返回自己, 即sb. 这里面, sb和sb2是指向的同一个内存区域, 同一个容器.
+        // 因为这里不是new一个StringBuilder容器
         StringBuilder sb2 = sb.append("hello");
 
         System.out.println("sb: " + sb);
