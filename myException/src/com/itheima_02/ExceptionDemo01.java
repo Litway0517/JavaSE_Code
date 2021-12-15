631c9bb386057fff628fd99f4e61319d0298425e
Mingw0717-Dell <xmw0517@gmail.com>
2021-12-15 23:03:52 +0800

    master -> myException module try...catch捕捉异常, 这样会继续执行其他的代码

---
+++ myException/src/com/itheima_02/ExceptionDemo01.java
@@ -0,0 +1,24 @@
+package com.itheima_02;
+
+public class ExceptionDemo01 {
+    public static void main(String[] args) {
+
+        //
+        System.out.println("start");
+        method();
+        System.out.println("end");
+
+
+    }
+
+    public static void method() {
+        int[] arr = {1, 3, 3};
+        // try...catch
+        try {
+            System.out.println(arr[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("访问的数组信息不存在");   // 实际上得给用户一个界面提示, 但是现在还给不了
+            e.printStackTrace();
+        }
+    }
+}
