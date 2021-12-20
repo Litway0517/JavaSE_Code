40e10260924818804ed05d6c9a878ae5d5ec67fc
Mingw0717-Dell <xmw0517@gmail.com>
2021-12-20 23:02:41 +0800

    master -> myCollection module Collection是一个接口 这里通过ArrayList来实例化

---
+++ myCollection/src/com/itheima_01/CollectionDemo01.java
@@ -0,0 +1,19 @@
+package com.itheima_01;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo01 {
+    public static void main(String[] args) {
+
+        // 创建Collection对象
+        // Collection是接口, 不能直接实例化, 因此, 这里是通过 实现了Collection这个接口的类ArrayList实例化的. 
+        Collection<String> c = new ArrayList<String>();
+
+        // 添加元素
+        c.add("Hello");
+        c.add("World");
+        c.add("JavaSE");
+        System.out.println(c);
+    }
+}
