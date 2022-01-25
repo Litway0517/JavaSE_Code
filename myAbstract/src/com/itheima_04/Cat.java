5b8d232d3c71ba1bf36cea357b4b10ad3bea1acd
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-25 22:31:07 +0800

    myAbstract

--- myAbstract/src/com/itheima_04/Cat.java
+++ myAbstract/src/com/itheima_04/Cat.java
@@ -1,14 +1,32 @@
 package com.itheima_04;
 
-public class Cat extends Animal{
+/**
+ * 猫
+ *
+ * @author DELL_
+ * @date 2022/01/25
+ */
+public class Cat extends Animal {
 
+    /**
+     * 猫
+     */
     public Cat() {
     }
 
+    /**
+     * 猫
+     *
+     * @param name 名字
+     * @param age  年龄
+     */
     public Cat(String name, int age) {
         super(name, age);
     }
 
+    /**
+     * 吃
+     */
     @Override
     public void eat() {
         System.out.println("猫抓老鼠");
--- myAbstract/src/com/itheima_04/Dog.java
+++ myAbstract/src/com/itheima_04/Dog.java
@@ -1,14 +1,32 @@
 package com.itheima_04;
 
+/**
+ * 狗
+ *
+ * @author DELL_
+ * @date 2022/01/25
+ */
 public class Dog extends Animal {
 
+    /**
+     * 狗
+     */
     public Dog() {
     }
 
+    /**
+     * 狗
+     *
+     * @param name 名字
+     * @param age  年龄
+     */
     public Dog(String name, int age) {
         super(name, age);
     }
 
+    /**
+     * 吃
+     */
     @Override
     public void eat() {
         System.out.println("狗啃骨头");
