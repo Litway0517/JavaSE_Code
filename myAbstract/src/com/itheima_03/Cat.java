e5ef577668d2dccee45e7bef50555f8367da04bf
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-25 22:28:39 +0800

    myAbstract 抽象类中的具体方法的内部变量和抽象类本身的外部成员变量名重复时 以方法内的局部变量为准

--- myAbstract/src/com/itheima_03/Cat.java
+++ myAbstract/src/com/itheima_03/Cat.java
@@ -1,7 +1,16 @@
 package com.itheima_03;
 
+/**
+ * 猫
+ *
+ * @author DELL_
+ * @date 2022/01/25
+ */
 public class Cat extends Animal{
 
+    /**
+     * 吃
+     */
     @Override
     public void eat() {
         System.out.println("猫抓老鼠");
