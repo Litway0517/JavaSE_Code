587a454376abb4c9264c1e5c9b6e5d79edc6b42b
Litway_ <xmw0517@gmail.com>
2022-09-08 15:08:42 +0800

    myInterfaceOne

--- myInterfaceOne/src/com/itheima_02/InterImpl.java
+++ myInterfaceOne/src/com/itheima_02/InterImpl.java
@@ -3,8 +3,12 @@ package com.itheima_02;
 public class InterImpl implements Inter {
 
     public InterImpl() {
-        super();        // 这个super();是无参构造方法调用, 但是并不是调用的Inter这个接口里面, 因为接口本身不能写构造方法, 自然不能被调用.
-    }                   // 实际上是调用的Object这个类的无参构造方法(提示: Object这个类只有无参构造方法-->这也是为什么JVM会自动调用超类的无参构造方法)
+        /*
+            这个super();是无参构造方法调用, 但是并不是调用的Inter这个接口里面, 因为接口本身不能写构造方法, 自然不能被调用.
+            实际上是调用的Object这个类的无参构造方法(提示: Object这个类只有无参构造方法-->这也是为什么JVM会自动调用超类的无参构造方法)
+         */
+        super();
+    }
 
 
     // 必须重写接口中的抽象方法
